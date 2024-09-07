a5de74752044e0734b07327d9ee7658a53bf9603
PEDRINHOMAISLINDO <Pedro.GitHub7@gmail.com>
2024-09-07 15:08:08 -0300

    ÁREA DE UM TRIÂNGULO

---
+++ EXERCÍCO_06.py
@@ -0,0 +1,17 @@
+#6) Escreva um algoritmo para ler as dimensões de um retângulo (base e altura),
+# calcular e escrever a área do retângulo.
+
+# ÁREA DE UM TRIÂNGULO :
+from colorama import Fore, init
+from time import sleep
+print('=-' * 25)
+print('ÁREA DE UM TRIÂNGULO: ')
+print('=-' * 25)
+sleep(2)
+base = int(input('QUAL É A BASE DO TRIÂNGULO: '))
+altura = int('QUAL É A ALTURA DESSE TRIÂNGULO: ')
+area = base * altura
+print(f'A ÁREA DO SEU TRIÂNGULOI VAI SER DE {base}')
+
+
+
