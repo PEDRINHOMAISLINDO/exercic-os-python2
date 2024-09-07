15effca061bd90ce48cfe6e3dfc6206e69532a2d
PEDRINHOMAISLINDO <Pedro.GitHub7@gmail.com>
2024-09-07 15:22:26 -0300

    FAREHNHEIT PARA CÉLSIUS
    Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius

---
+++ EXERCÍCIO_12.py
@@ -0,0 +1,8 @@
+print('=-' * 25)
+print('FAHRENHEIT PARA CÉLSIUS: ')
+print('=-' * 25)
+fare = float(input('DIGITE UM NÚMERO: '))
+print('=-' * 25)
+calculo = (fare - 32) / 1.8
+print(f'O RESULTADO EM CÉLSIUS É {calculo}ºC')
+exit()
