f0bf24fff1da878e11cbc0ef7dfe30e421dc98ce
PEDRINHOMAISLINDO <Pedro.GitHub7@gmail.com>
2024-09-30 15:00:59 -0300

    EXERCÍCIO_28.py

--- EXERCÍCIO_28.py
+++ EXERCÍCIO_28.py
@@ -2,16 +2,16 @@ from colorama import init, Fore # BIBLIOTECA PARA MUDAR AS CORES DAS LETRAS
 init()# SERIA PARA INICIAR O PROGRAMA COLORAMA.
 
 print('=-' * 25) # MULTIPLICANDO A STR "=-" POR 25.
-n1 = float(input('DIGITE UM NÚMERO: '))
+n1 = int(input('DIGITE UM NÚMERO: '))
 print('=-' * 25)
-n2 = float(input('DIGITE UM SEGUNDO NÚMERO: '))
+n2 = int(input('DIGITE UM SEGUNDO NÚMERO: '))
 print('=-' * 25)
-n3 = float(input('DIGITE UM TERCEIRO NÚMERO: '))
+n3 = int(input('DIGITE UM TERCEIRO NÚMERO: '))
 print('=-' * 25)
 
-while n1 == n2 and n2 == n3: # ESTRUTURA DE REPETIÇÃO PARA IMPEDIR QUE TODOS OS NÚMEROS SEJAM IGUAIS.
+while n1 == n2 or n2 == n3 or n1 == n3: # ESTRUTURA DE REPETIÇÃO PARA IMPEDIR QUE TODOS OS NÚMEROS SEJAM IGUAIS.
     print(Fore.LIGHTRED_EX + 'NÃO DIGITE NÚMEROS IGUAIS!')# Fore.LIGHTRED_EX SERIA UMA FORMA DE ATRIBUIR UMA COR A UMA STR .
-    n1 = float(input('DIGITE UM NÚMERO: '))
+    n1 = float(input(Fore.RESET + 'DIGITE UM NÚMERO: '))
     print('=-' * 25)
     n2 = float(input('DIGITE UM SEGUNDO NÚMERO: '))
     print('=-' * 25)
